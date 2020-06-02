cb0d635489e15d36babe962a9bf129b684cef190
Rita Roloff <ritaroloff@Ritas-MacBook-Air.local>
2020-06-02 11:30:20 -0700

    my first commit

---
+++ quiz.js
@@ -0,0 +1,84 @@
+const question = document.getElementById("question");//gets the question
+const choices = Array.from(document.getElementsByClassName("choice-text")); //gets the choice options
+const progressText = document.getElementById("progressText");
+const progressBarFull = document.getElementById("progressBarFull");
+
+let currentQuestion ={};
+let acceptingAnswers = false;
+let questionCounter = 0; //tracks the question number we're on
+let availableQuestions = []; //tracks the number of questions left
+
+
+let questions =[
+    {
+      question: "Inside which HTML element do we put the JavaScript??",
+      choice1: "<script>",
+      choice2: "<javascript>",
+      choice3: "<js>",
+    },
+    {
+      question:
+        "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choice1: "<script href='xxx.js'>",
+      choice2: "<script name='xxx.js'>",
+      choice3: "<script src='xxx.js'>",
+    },
+    {
+      question: " How do you write 'Hello World' in an alert box?",
+      choice1: "msgBox('Hello World');",
+      choice2: "alertBox('Hello World');",
+      choice3: "msg('Hello World');",
+    }
+];
+
+
+//CONSTANTS
+const MAX_QUESTIONS = 5;
+
+startQuiz = () => {
+    questionCounter = 0;
+    availableQuestions = [... questions];
+    getNewQuestion();
+}
+
+  
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0) {
+      //go to the end page
+      return window.location.assign("/end.html");
+    }
+
+    
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    const questionIndex = 0;
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+  
+    choices.forEach(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
+    });
+  
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+  };
+  
+  choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+      if (!acceptingAnswers) return;
+  
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
+      console.log(selectedAnswer);
+      //save selected answer to database
+      getNewQuestion();
+    });
+  });
+
+
+startQuiz();
